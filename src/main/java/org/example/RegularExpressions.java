35be2225fbd676e2484b1ea093e2359565102d05
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2024-06-18 10:59:33 +0300

    implemented second task

--- src/main/java/org/example/RegularExpressions.java
+++ src/main/java/org/example/RegularExpressions.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegularExpressions {
     public static boolean task1(String s) {
-        return s.matches("^automation_[0-9]{8}_(.){6}@solvd.com");
+        return s.matches("^automation_\\d{8}_(.){6}@solvd.com");
+    }
+
+    public static List<String> task2(String s) {
+        String replacedCharsStr = s.replaceAll("[+() -]","");
+        Pattern pattern = Pattern.compile("1\\d{10}");
+        Matcher matcher = pattern.matcher(replacedCharsStr);
+        ArrayList<String> list = new ArrayList<>();
+        while (matcher.find()) {
+            list.add(replacedCharsStr.substring(matcher.start(), matcher.end()));
+        }
+        return  list;
     }
 }
--- src/test/java/RegularExpressionsTest.java
+++ src/test/java/RegularExpressionsTest.java
@@ -3,20 +3,39 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class RegularExpressionsTest {
 
     @Test(dataProvider = "task1dp")
     public void testTask1(String s, boolean expectedResult) {
-        Assert.assertEquals(RegularExpressions.task1(s),expectedResult);
+        Assert.assertEquals(RegularExpressions.task1(s), expectedResult);
     }
 
     @DataProvider(name = "task1dp")
     public Object[][] dataProviderTask1() {
-        return new Object[][] {
-                {"automation_20240325_a1b2c3@solvd.com",true},
-                {"automation_test_email@solvd.com",false},
-                {"automation_20240325_a1b2c3@solvd.comm",false},
-                {"automation_2024032e_a1b2c3@solvd.com",false}
+        return new Object[][]{
+                {"automation_20240325_a1b2c3@solvd.com", true},
+                {"automation_test_email@solvd.com", false},
+                {"automation_20240325_a1b2c3@solvd.comm", false},
+                {"automation_2024032e_a1b2c3@solvd.com", false}
+        };
+    }
+
+    @Test(dataProvider = "task2dp")
+    public void testTask2(String s, List<String> expectedResult) {
+        Assert.assertEquals(RegularExpressions.task2(s), expectedResult);
+    }
+
+    @DataProvider(name = "task2dp")
+    public static Object[][] task2dp() {
+        return new Object[][]{
+                {"Contact us at +1 (123) 456-7890 or 1-777-123-4567",
+                        List.of("11234567890","17771234567")
+                },
+                {"Contact us at +1 (123) 456-7890 1-777-123-4567",
+                        List.of("11234567890","17771234567")
+                }
         };
     }
 }
